00f3127c55a76cd506fb90b091dabbd9f0f29e0f
100472173 <100472173@alumnos.uc3m.es>
2023-05-24 13:11:51 +0200

    a

--- clase_termostato.py
+++ clase_termostato.py
@@ -2,13 +2,12 @@ from Csv import Csv
 
 
 class Termostato:
-    def __init__(self, temp_inicial: float, costeON: int, costeOFF: int):
+    def __init__(self, costeON: int, costeOFF: int):
         # Indicamos las distintas temperaturas y sus probabilidades.
         self.temperaturas = [16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22, 22.5, 23, 23.5,
                              24, 24.5, 25]
         # Los costes y la temperatura inicial se chequean.
-        self.__temp_inicial, self.__costeON, self.__costeOFF = self.check_valores(temp_inicial,
-                                                                                  costeON, costeOFF)
+        self.__costeON, self.__costeOFF = self.check_valores(costeON, costeOFF)
         self.iteracion = 0
         # De estos archivos csv se sacan los diccionarios de probabilidades (uno para ON y otro para OFF).
         self.lector = Csv("probON.csv", "probOFF.csv")
@@ -18,16 +17,9 @@ class Termostato:
                               "19.5": 0, "20": 0, "20.5": 0, "21": 0, "21.5": 0, "22": 0,
                               "22.5": 0, "23": 0, "23.5": 0, "24": 0, "24.5": 0, "25": 0}
 
-        self.ruta = {"16": [], "16.5": [], "17": [], "17.5": [], "18": [], "18.5": [], "19": [],
-                     "19.5": [], "20": [], "20.5": [], "21": [], "21.5": [], "22": [],
-                     "22.5": [], "23": [], "23.5": [], "24": [], "24.5": [], "25": []}
-
-    def proceso_optimo(self):
-        """Devuelve el peso, ruta y el num_iteraciones del estado elegido como inicial"""
-        self.ecuacion_Bellman()
-        peso_temperatura = self.valor_estados[str(self.__temp_inicial)]
-        ruta_temperatura = self.ruta[str(self.__temp_inicial)]
-        return peso_temperatura, ruta_temperatura, self.iteracion
+        self.ruta = {"16": "", "16.5": "", "17": "", "17.5": "", "18": "", "18.5": "", "19": "",
+                     "19.5": "", "20": "", "20.5": "", "21": "", "21.5": "", "22": "No computable, estado meta",
+                     "22.5": "", "23": "", "23.5": "", "24": "", "24.5": "", "25": ""}
 
     def ecuacion_Bellman(self):
         """Realiza la ecuación de Bellman según los costes elegidos para todos los estados."""
@@ -43,7 +35,7 @@ class Termostato:
                 valorOFF = self.__costeOFF + self.prob_OFF["16"]["+0.5"] * self.valor_estados["16.5"] \
                            + self.prob_OFF["16"]["+0"] * self.valor_estados["16"]
                 next_valor, paso = self.metodo_usado(valorON, valorOFF)
-                self.ruta["16"].append(paso)
+                self.ruta["16"] = paso
                 nuevos_valores.append(next_valor)
 
             elif elemento == 22:
@@ -58,7 +50,7 @@ class Termostato:
                            + self.prob_OFF["24.5"]["+0"] * self.valor_estados["24.5"] \
                            + self.prob_OFF["24.5"]["-0.5"] * self.valor_estados["24"]
                 next_valor, paso = self.metodo_usado(valorON, valorOFF)
-                self.ruta["24.5"].append(paso)
+                self.ruta["24.5"] = paso
                 nuevos_valores.append(next_valor)
 
             elif elemento == 25:
@@ -67,7 +59,7 @@ class Termostato:
                 valorOFF = self.__costeOFF + self.prob_OFF["25"]["+0"] * self.valor_estados["25"] \
                            + self.prob_OFF["25"]["-0.5"] * self.valor_estados["24.5"]
                 next_valor, paso = self.metodo_usado(valorON, valorOFF)
-                self.ruta["25"].append(paso)
+                self.ruta["25"] = paso
                 nuevos_valores.append(next_valor)
             else:
                 # Se calcula el estado anterior en la lista y los dos siguientes porque son los que
@@ -88,35 +80,30 @@ class Termostato:
                            + self.prob_OFF[actual]["-0.5"] * self.valor_estados[anterior]
                 next_valor, paso = self.metodo_usado(valorON, valorOFF)
                 # Se añade la elección a la ruta
-                self.ruta[actual].append(paso)
+                self.ruta[actual] = paso
                 nuevos_valores.append(next_valor)
-            # Las iteraciones se suman si el valor del estado inicial se modifica
-            if elemento == self.__temp_inicial:
-                next_valor = round(nuevos_valores[contador], 2)
-                if next_valor != self.valor_estados[str(elemento)]:
-                    self.iteracion += 1
             contador += 1
-
-        # En comparar_valores, analizamos si los nuevos valores son idénticos o no a los anteriores.
+        # En comparar_valores valoramos si se han estabilizado los valores esperados. En nuestro caso la tolerancia es
+        # bastante baja, hemos elegido que sea 0.001, es decir, que los valores, que tienen dos decimales sean identicos
         fin = self.comparar_valores(nuevos_valores)
         contador = 0
-        # Se actualizan los pesos
+        # Se actualizan los valores esperados
         for elemento in self.temperaturas:
             self.valor_estados[str(elemento)] = round(nuevos_valores[contador], 2)
             contador += 1
-        # Si no es el fin, se repite de forma recursiva el proceso.
         if not fin:
+            # Como los valores no se han estabilizado con respecto a la tolerancia elegida, repetimos el proceso
+            # recursivamente.
+            self.iteracion += 1
             self.ecuacion_Bellman()
 
-    def check_valores(self, temperatura, costeON, costeOFF):
+    def check_valores(self, costeON, costeOFF):
         """Función para checkear """
-        if temperatura not in self.temperaturas:
-            raise ValueError("Temperatura no perteneciente al conjunto de estados.")
         if costeON < 1:
             raise ValueError("El coste de encendido debe ser positivo.")
         if costeOFF < 1:
             raise ValueError("El coste de apagado debe ser positivo.")
-        return temperatura, costeON, costeOFF
+        return costeON, costeOFF
 
     def comparar_valores(self, nuevos_valores):
         """Para ver si se repite o no el proceso"""
@@ -133,5 +120,5 @@ class Termostato:
     def metodo_usado(valorON, valorOFF):
         """Para devolver si el termostato está ON o OFF"""
         if valorOFF < valorON:
-            return valorOFF, "OFF"
-        return valorON, "ON"
+            return valorOFF, "Apagar"
+        return valorON, "Encender"
--- ejecutor_pruebas.py
+++ ejecutor_pruebas.py
@@ -1,16 +1,13 @@
 from clase_termostato import Termostato
-temperatura = input("Escribe un número del 16 al 25:")
-if "." in temperatura:
-    temperatura = float(temperatura)
-else:
-    temperatura = int(temperatura)
 
 costeON = int(input("Escribe el coste del termostato encendido:"))
 costeOFF = int(input("Escribe el coste del termostato apagado:"))
-print("Para temperatura", temperatura, ", coste encendido", costeON, "y coste apagado", costeOFF,
-      "; tenemos los siguientes resultados:")
-prueba = Termostato(temperatura, costeON, costeOFF)
-peso, ruta, num_iteraciones = prueba.proceso_optimo()
-print("->Num_iteraciones:", num_iteraciones)
-print("->Peso total:", peso)
-print("->Ruta realizada:", ruta)
+lista_temperaturas = [16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22, 22.5, 23, 23.5, 24, 24.5, 25]
+termostato = Termostato(costeON, costeOFF)
+termostato.ecuacion_Bellman()
+print("La tolerancia usada para nuestras pruebas es 0.001")
+print("Las iteraciones realizadas hasta la estabilización de los valores esperados son: ", termostato.iteracion)
+print("El vector de pesos asociado al MDP es el siguiente:")
+print(termostato.valor_estados)
+print("El vector de políticas óptimas es el siguiente:")
+print(termostato.ruta)
